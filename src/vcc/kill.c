48cf03fad8988ee86fb674cb06ff903b97bc622b
Luca Longinotti <chtekk@gentoo.org>
2007-02-11 22:38:33 +0000

    Fix vcc kill and vcc help text.

--- src/vcc/kill.c
+++ src/vcc/kill.c
@@ -15,11 +15,25 @@
 // Free Software Foundation, Inc.,
 // 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+#include <stdlib.h>
+
+#include <lucid/scanf.h>
+
 #include "cmd.h"
 
 void cmd_kill(xmlrpc_env *env, int argc, char **argv)
 {
+	int pid = 0, sig = 0;
+
+	if (argc < 2)
+		usage(EXIT_FAILURE);
+
+	sscanf(argv[0], "%d", &pid);
+	sscanf(argv[1], "%d", &sig);
+
 	xmlrpc_client_call(env, uri, "vx.kill",
-		SIGNATURE("{s:s}"),
-		"name", name);
+		SIGNATURE("{s:s,s:i,s:i}"),
+		"name", name,
+		"pid", pid,
+		"sig", sig);
 }
